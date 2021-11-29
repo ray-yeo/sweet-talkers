5b9fe00ba4ea47dba23734f98848b4fb917077fa
geireann <60007097+geireann@users.noreply.github.com>
2021-11-29 14:17:04 -0500

    updates

--- model.py
+++ model.py
@@ -1,4 +1,14 @@
+import numpy as np
+
+import tensorflow as tf
+import tensorflow_hub as hub
+
+import matplotlib.pyplot as plt
+print("TensorFlow Version: ", tf.__version__)
+
 from preprocess import get_data
+
+
 
 
 def train():
@@ -11,7 +21,59 @@ def test():
 
 def main():
     X_train, X_test, y_train, y_test = get_data("jsonoutput.csv")
+    model = "https://tfhub.dev/google/nnlm-en-dim50/2"
+    hub_layer = hub.KerasLayer(model, input_shape=[], dtype=tf.string, trainable=True)
+    hub_layer(X_train[:3])
 
+    model = tf.keras.Sequential()
+    model.add(hub_layer)
+    model.add(tf.keras.layers.Dense(16, activation='relu'))
+    model.add(tf.keras.layers.Dense(1))
+
+    model.summary()
+
+    model.compile(optimizer='adam',
+              loss=tf.losses.BinaryCrossentropy(from_logits=True),
+              metrics=[tf.metrics.BinaryAccuracy(threshold=0.0, name='accuracy')])
+
+    x_val = X_train[:300]
+    partial_x_train = X_train[300:]
+
+    y_val = y_train[:300]
+    partial_y_train = y_train[300:]
+
+    history = model.fit(partial_x_train,
+                    partial_y_train,
+                    epochs=40,
+                    batch_size=512,
+                    validation_data=(x_val, y_val),
+                    verbose=1)
+
+    results = model.evaluate(X_test, y_test)
+
+    print(results)
+
+
+    history_dict = history.history
+    history_dict.keys()
+
+    acc = history_dict['accuracy']
+    val_acc = history_dict['val_accuracy']
+    loss = history_dict['loss']
+    val_loss = history_dict['val_loss']
+
+    epochs = range(1, len(acc) + 1)
+
+    # "bo" is for "blue dot"
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    # b is for "solid blue line"
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    plt.show()
 
 if __name__ == '__main__':
     main()
--- preprocess.py
+++ preprocess.py
@@ -1,10 +1,17 @@
 import csv
 import sklearn
 import sklearn.model_selection
+import math
 
 """
 split csv file into inputs_list and labels_list
 """
+
+def round(x):
+    if (int(float(x)) >= 4):
+        return 1
+    else:
+        return 0
 
 def get_data(filename = 'jsonoutput.csv'):
     # open file and read in rows
@@ -27,8 +34,9 @@ def get_data(filename = 'jsonoutput.csv'):
         inputs_list.append(inputs)
         labels_list.append(labels)
 
-    print(inputs_list[0])
-    print(labels_list[0])
+    labels_list = list(map(round,labels_list))
+    # print(inputs_list[:10])
+    print(labels_list[:10])
 
     # Use sklearn to do train and test split 0f 0.25 | 0.75
     X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(inputs_list, labels_list, test_size=0.25, random_state=0)
